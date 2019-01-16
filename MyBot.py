266a4fa13f4ce8d26dbf56da1ab30029048fbfa8
Ryan S. Walt <waltr@mail.gvsu.edu>
2019-01-16 00:33:24 -0500

    BIG CHANGE

--- MyBot.py
+++ MyBot.py
@@ -37,7 +37,7 @@ count = 10
 width = game.game_map.width
 height = game.game_map.height
 command_list = []
-ship_list = [] 
+ship_list = []
 target_list = []
 move_list = []
 
@@ -46,13 +46,14 @@ shipRange = 6
 
 
 class Miner:
-    def __init__(self, shipId):
+    def __init__(self, shipId, type):
         self.targetX = 0
         self.targetY = 0
         self.ship = me.get_ship(shipId)
         self.hasTarget = False
         self.headingHome = False
         self.avoiding = False
+        self.type = type
 
     def target_highest(self):
         #Dont seek the new highest if we are already seeking a target (Adjust later to handle
@@ -113,13 +114,26 @@ class Miner:
 
     #Seconds time is the charm?
     def seek2(self):
-        self.target_highest()
+        if self.type == 0:
+            self.target_highest()
+        else:
+            self.target_ideal()
+
         self.avoiding = False
-        # self.target_ideal()
+
         x = self.ship.position.x
         y = self.ship.position.y
         desireX = x
         desireY = y
+
+        if game.game_map[hlt.entity.Position(x, y)].halite_amount > 50 and self.ship.halite_amount < 999:
+            command_list.append(self.ship.stay_still())
+            return
+        if self.ship.halite_amount > 980:
+            if self.headingHome is False:
+                self.clearTarget()
+                self.target_home()
+
 
         ideal_moves = []
         possible_moves = []
@@ -140,21 +154,35 @@ class Miner:
             if self.headingHome is True:
                 self.headingHome = False
                 self.hasTarget = False
+                return
 
-            #Over 900 halite? Start to head back
-            elif self.ship.halite_amount > 900:
-                if self.headingHome is False:
-                    self.target_home()
+            if self.type == 0:
+                if self.ship.halite_amount > 980 or game.game_map[hlt.entity.Position(self.targetX, self.targetY)].halite_amount < 20:
+                    if self.headingHome is False:
+                        self.clearTarget()
+                        self.target_home()
 
-            # Space less than 20? Head to another
-            elif game.game_map[hlt.entity.Position(self.targetX, self.targetY)].halite_amount < 20:
-                self.hasTarget = False
-                self.target_ideal()
+                #Otherwise continue to collect halite
+                else:
+                    command_list.append(self.ship.stay_still())
+                return
 
-            #Otherwise continue to collect halite
             else:
-                command_list.append(self.ship.stay_still())
-            return
+                if self.ship.halite_amount > 980:
+                    if self.headingHome is False:
+                        self.clearTarget()
+                        self.target_home()
+
+                # Space less than 20? Head to another
+                elif game.game_map[hlt.entity.Position(self.targetX, self.targetY)].halite_amount < 20:
+                    self.hasTarget = False
+                    self.clearTarget()
+                    self.target_ideal()
+
+                #Otherwise continue to collect halite
+                else:
+                    command_list.append(self.ship.stay_still())
+                return
 
         #Pick a random valid move and see if we can move in that direction.
         while len(ideal_moves) != 0:
@@ -219,6 +247,12 @@ class Miner:
                 return True
         return False
 
+    def clearTarget(self):
+        for coord in target_list:
+            if self.targetX == coord[0] and self.targetY == coord[1]:
+                target_list.remove(coord)
+
+
 def checkDead():
     if len(me.get_ships()) != len(ship_list):
         for myShip in ship_list:
@@ -231,6 +265,17 @@ def checkDead():
             if idFound is False:
                 ship_list.remove(myShip)
                 command_list = []
+
+def spawnShip():
+    # Spawn a new ship for this temp condition
+    if len(me.get_ships()) < 15 and me.halite_amount > 1000:
+        if game.game_map[hlt.entity.Position(me.shipyard.position.x, me.shipyard.position.y)].is_occupied is False and safeSpawn is True:
+            command_list.append(me.shipyard.spawn())
+            newSpawn = True
+            tempList = []
+            for i in me.get_ships():
+                tempList.append(i.id)
+            logging.info("ship_list: {}".format(tempList))
 
 while True:
     #Update the frame
@@ -247,7 +292,7 @@ while True:
 
     safeSpawn = True
     for curShip in ship_list:
-        if game.game_map.calculate_distance(hlt.entity.Position(curShip.ship.position.x, curShip.ship.position.y), hlt.entity.Position(me.shipyard.position.x, me.shipyard.position.y)) < 3:
+        if game.game_map.calculate_distance(hlt.entity.Position(curShip.ship.position.x, curShip.ship.position.y), hlt.entity.Position(me.shipyard.position.x, me.shipyard.position.y)) < 1.5:
             safeSpawn = False
             break
 
@@ -263,36 +308,12 @@ while True:
                     found = True
                     break
             if found is False:
-                ship_list.append(Miner(newShip.id))
+                ship_list.append(Miner(newShip.id, 0))
         newSpawn = False
 
-
-    #Spawn a new ship for this temp condition
-    if len(me.get_ships()) < 8 and me.halite_amount > 1000:
-        if game.game_map[hlt.entity.Position(me.shipyard.position.x, me.shipyard.position.y)].is_occupied is False and safeSpawn is True:
-            command_list.append(me.shipyard.spawn())
-            newSpawn = True
-            tempList = []
-            for i in me.get_ships():
-                tempList.append(i.id)
-            logging.info("ship_list: {}".format(tempList))
-
+    spawnShip()
 
     for tempShip in ship_list:
         tempShip.seek2()
 
-
-    # Send your moves back to the game environment, ending this turn.
-    # tempMoves = []
-    # for i in command_list:
-    #     sep = i.split(" ")
-    #     if len(sep) > 1:
-    #         id = sep[1]
-    #         for move in tempMoves:
-    #             if move == id:
-    #                 logging.info("REMOVED A DUPLICATE")
-    #                 command_list.remove(i)
-    #         tempMoves.append(id)
-        # logging.info("Move: {}".format(i))
-        # logging.info("Type: {}".format(type(i)))
     game.end_turn(command_list)
